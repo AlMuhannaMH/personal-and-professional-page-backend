2b52426ffa9f39f3778fe703a6c8e7a54dbbfb6c
Lamooooo1418 <lamooooo1418@gmail.com>
2020-03-15 15:00:37 +0300

    remove .body from Server

--- Server/app/routes/user.routes.js
+++ Server/app/routes/user.routes.js
@@ -145,13 +145,13 @@ router.delete("/sign-out", requireToken, (req, res, next) => {
     .catch(next);
 });
 
-router.patch("/api/Users/:id", (req, res) => {
+router.patch("/Users/:id", (req, res) => {
   User.findById(req.params.id)
     .then(user => {
       if (user) {
         // Pass the result of Mongoose's `.update` method to the next `.then`
         res.status(201).json({ user });
-        return user.update(req.body.user);
+        return user.update(req.user);
       } else {
         // If we couldn't find a document with the matching ID
         res.status(404).json({
