d381e8fc418622f72e73cc117a7a43758b1a4f2b
Mohammed H. AlMuhanna <almuhannamh@outlook.com>
2020-03-15 21:22:26 +0300

    Update serverJS and resume.routersJS from Renad

--- Server/server.js
+++ Server/server.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 // require route files
 const exampleRoutes = require('./app/routes/example_routes')
+const resumeRoutes = require('./app/routes/resume.routes')
 const userRoutes = require('./app/routes/user.routes')
 
 // require error handling middleware
@@ -64,6 +65,7 @@ app.use(requestLogger)
 
 // register route files
 app.use(exampleRoutes)
+app.use(resumeRoutes)
 app.use(userRoutes)
 
 // register error handling middleware
