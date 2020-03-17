03ef02a2b6f87010a9a10a38d86a17fd940f6f99
Mohammed H. AlMuhanna <almuhannamh@outlook.com>
2020-03-17 23:02:09 +0300

    update resume routers, team work.

--- Server/app/routes/resume.routes.js
+++ Server/app/routes/resume.routes.js
@@ -1,214 +1,126 @@
-const express = require('express')
+// Require necessary NPM packages
+const express = require('express');
 
+// Require Mongoose Model for Resume
+const Resume = require('../models/resume.model');
 
-const User = require('../models/user.model')
-const Resume = require('../models/resume.model')
-
+// Instantiate a Router (mini app that only handles routes)
 const router = express.Router();
 
-///// Add New Resume to User Account /////
-router.post('/resume', (req, res) => {
-
-    let user;
-    // find a user based on the token 
-    User.findOne({
-        token: req.headers.token
-    })
-        .then(record => {
-            if (!record) {
-                res.status(401).json({
-                    error: {
-                        name: 'Unauthorized',
-                        message: 'The provided cridintials are not valid for this operation'
-                    } 
-                })
-            }
-            else{
-                user = record
-                //create new resume
-                Resume.create(req.body)
-                .then((newResume) => {
-                    user.resume.push(newResume._id)
-                    user.save()
-                    res.status(201).json({
-                        "newResume": newResume
-                    });
-                })
-            } 
+/**
+ * Action:        INDEX
+ * Method:        GET
+ * URI:           /resumes
+ * Description:   Get All Resumes
+ */
+router.get('/resumes', (req, res) => {
+    Resume.find()
+        // Return all Resumes as an Array
+        .then((allResumes) => {
+            res.status(200).json({ resumes: allResumes });
         })
         // Catch any errors that might occur
         .catch((error) => {
-            res.status(500).json({
-                error: error
-            });
+            res.status(500).json({ error });
         });
 });
 
-
-///// Show all User Resumes /////
-router.get('/resume', (req, res) => {
-    // find a user based on the token 
-    User.findOne({
-        token: req.headers.token
-    })
-        .then(record => {
-            if (!record) {
-                res.status(401).json({
-                    error: {
-                        name: 'Unauthorized',
-                        message: 'The provided cridintials are not valid for this operation'
-                    }
-                });
-            } else {
-                console.log(record.resume);
-
-
-                Resume.find({
-                    _id:
-                        { $in: record.resume }
-                })
-                    // Return all Resume as an Array
-                    .then((resume) => {
-                        console.log(resume);
-
-                        res.status(200).json({
-                            resume: resume
-                        });
-                    })
-
-            }
-            user = record
-        })
-        // Catch any errors that might occur
-        .catch((error) => {
-            res.status(500).json({
-                error: error
-            });
-        });
-});
-
-
-
-///// Show One of User's Resume /////
-router.get('/resume/:id', (req, res) => {
-    // find a user based on the token 
-    User.findOne({
-        $and: [{
-            token: req.headers.token
-        },
-        {
-            resume: req.params.id
-        }
-        ]
-    })
-        .then(record => {
-            if (!record) {
-                res.status(401).json({
-                    error: {
-                        name: 'Unauthorized',
-                        message: 'The provided cridintials are not valid for this operation'
-                    }
-                });
-            } else {
-                Resume.findById(req.params.id)
-                    .then((resume) => {
-                        if (resume) {
-                            res.status(200).json({
-                                resume: resume
-                            });
-
-                        } else {
-                            // If we couldn't find a document with the matching ID
-                            res.status(404).json({
-                                error: {
-                                    name: 'DocumentNotFoundError',
-                                    message: 'The provided ID doesn\'t match any documents'
-                                }
-                            });
-                        }
-                    })
-            }
-        })
-        // Catch any errors that might occur
-        .catch((error) => {
-            res.status(500).json({
-                error: error
-            });
-        })
-});
-
-
-
-///// Delete a Resume From User Acaount /////
-router.delete('/resume/:id', (req, res) => {
-    let user;
-    // find a user based on the token 
-    User.findOne({
-        $and: [{
-            token: req.headers.token
-        },
-        {
-            resume: req.params.id
-        }
-        ]
-    })
-
-        .then(record => {
-            console.log(record);
-            if (!record) {
-                res.status(401).json({
-                    error: {
-                        name: 'Unauthorized',
-                        message: 'The provided cridintials are not valid for this operation'
-                    }
-                });
-            } else {
-                user = record;
-                Resume.findById(req.params.id)
-                    .then((resume) => {
-                        if (resume) {
-                            // Pass the result of Mongoose's `.delete` method to the next `.then`
-                            user.resume.splice(user.resume.indexOf((req.params.id, 0), 1))
-                            user.save()
-                            return resume.remove();
-                        } else {
-                            // If we couldn't find a document with the matching ID
-                            res.status(404).json({
-                                error: {
-                                    name: 'DocumentNotFoundError',
-                                    message: 'The provided ID Doesn\'t match any documents'
-                                }
-                            });
-                        }
-                    })
-                    .then(() => {
-                        // If the deletion succeeded, return 204 and no JSON
-                        res.status(204).end();
-                    })
-            }
-        })
-
-        // Catch any errors that might occur
-        .catch((error) => {
-            res.status(500).json({
-                error: error
-            });
-        });
-});
-
-router.patch('/resume/: id', (req, res) => {
+/**
+ * Action:        SHOW
+ * Method:        GET
+ * URI:           /resumes/5d664b8b68b4f5092aba18e9
+ * Description:   Get An Resume by Resume ID
+ */
+router.get('/resumes/:id', (req, res) => {
     Resume.findById(req.params.id)
-        .then(resume => {
+        .then((resume) => {
             if (resume) {
-                // Pass the result of Mongoose’s `.update` method to the next `.then`
-                res.status(201).json({ resume });
-                return user.update(req.body.resume);
+                res.status(200).json({ resume });
             } else {
-                // If we couldn’t find a document with the matching ID
+                // If we couldn't find a document with the matching ID
                 res.status(404).json({
                     error: {
                         name: 'DocumentNotFoundError',
-                        message: 'The provided ID Doesn’t match any documents'
+                        message: 'The provided ID doesn\'t match any documents'
+                    }
+                });
+            }
+        })
+        // Catch any errors that might occur
+        .catch((error) => {
+            res.status(500).json({ error });
+        })
+});
+
+/**
+* Action:       CREATE
+* Method:       POST
+* URI:          /resumes
+* Description:  Create a new Resume
+*/
+router.post('/resumes', (req, res) => {
+    Resume.create(req.body.resume)
+        // On a successful `create` action, respond with 201
+        // HTTP status and the content of the new resume.
+        .then((newResume) => {
+            res.status(201).json({ resume: newResume });
+        })
+        // Catch any errors that might occur
+        .catch((error) => {
+            res.status(500).json({ error });
+        });
+});
+
+/**
+ * Action:      UPDATE
+ * Method:      PATCH
+ * URI:         /resumes/5d664b8b68b4f5092aba18e9
+ * Description: Update An Resume by Resume ID
+ */
+router.patch('/resumes/:id', (req, res) => {
+    Resume.findById(req.params.id)
+        .then((resume) => {
+            if (resume) {
+                // Pass the result of Mongoose's `.update` method to the next `.then`
+                return resume.update(req.body.resume);
+            } else {
+                // If we couldn't find a document with the matching ID
+                res.status(404).json({
+                    error: {
+                        name: 'DocumentNotFoundError',
+                        message: 'The provided ID doesn\'t match any documents'
+                    }
+                });
+            }
+        })
+        .then(() => {
+            // If the update succeeded, return 204 and no JSON
+            res.status(204).end();
+        })
+        // Catch any errors that might occur
+        .catch((error) => {
+            res.status(500).json({ error });
+        });
+});
+
+/**
+* Action:       DESTROY
+* Method:       DELETE
+* URI:          /resumes/5d664b8b68b4f5092aba18e9
+* Description:  Delete An Resume by Resume ID
+*/
+router.delete('/resumes/:id', (req, res) => {
+    Resume.findById(req.params.id)
+        .then((resume) => {
+            if (resume) {
+                // Pass the result of Mongoose's `.delete` method to the next `.then`
+                return resume.remove();
+            } else {
+                // If we couldn't find a document with the matching ID
+                res.status(404).json({
+                    error: {
+                        name: 'DocumentNotFoundError',
+                        message: 'The provided ID Doesn\'t match any documents'
                     }
                 });
             }
@@ -218,9 +130,10 @@ router.patch('/resume/: id', (req, res) => {
             res.status(204).end();
         })
         // Catch any errors that might occur
-        .catch(error => {
-            res.status(500).json({ error: error });
+        .catch((error) => {
+            res.status(500).json({ error });
         });
 });
 
-module.exports = router
+// Export the Router so we can use it in the server.js file
+module.exports = router;
