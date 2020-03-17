96ffb12cf05935be33e909a4d85d1b4feba7189f
Mohammed H. AlMuhanna <almuhannamh@outlook.com>
2020-03-17 19:47:53 +0300

    add get to show user info, team work.

--- Server/app/routes/user.routes.js
+++ Server/app/routes/user.routes.js
@@ -157,31 +157,48 @@ router.delete("/sign-out", requireToken, (req, res, next) => {
     .catch(next);
 });
 
-// router.patch("/Users/:id", (req, res) => {
-//   User.findById(req.params.id)
-//     .then(user => {
-//       if (user) {
-//         // Pass the result of Mongoose's `.update` method to the next `.then`
-//         res.status(201).json({ user });
-//         return user.update(req.user);
-//       } else {
-//         // If we couldn't find a document with the matching ID
-//         res.status(404).json({
-//           error: {
-//             name: "DocumentNotFoundError",
-//             message: "The provided ID Doesn't match any documents"
-//           }
-//         });
-//       }
-//     })
-//     .then(() => {
-//       // If the deletion succeeded, return 204 and no JSON
-//       res.status(204).end();
-//     })
-//     // Catch any errors that might occur
-//     .catch(error => {
-//       res.status(500).json({ error: error });
-//     });
-// });
+///// Show One of User's Resume /////
+router.get('/:username', (req, res) => {
+  // find a user based on the token 
+  User.findOne({
+    {
+      username: req.params.username
+    }
+  })
+  .then(record => {
+    if (!record) {
+      res.status(401).json({
+        error: {
+          name: 'Unauthorized',
+          message: 'The provided cridintials are not valid for this operation'
+        }
+      });
+    } else {
+      User.findOne(req.params.username)
+        .then((profile) => {
+          if (profile) {
+            res.status(200).json({
+              profile: profile
+            });
+
+          } else {
+            // If we couldn't find a document with the matching ID
+            res.status(404).json({
+              error: {
+                name: 'DocumentNotFoundError',
+                message: 'The provided ID doesn\'t match any documents'
+              }
+            });
+          }
+        })
+    }
+  })
+  // Catch any errors that might occur
+  .catch((error) => {
+    res.status(500).json({
+      error: error
+    });
+  })
+});
 
 module.exports = router;
