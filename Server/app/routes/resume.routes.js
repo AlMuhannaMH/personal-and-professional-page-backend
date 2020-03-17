8025b3d46455d7d2e16f5c900dcb7d107e1a4bdc
Mohammed H. AlMuhanna <almuhannamh@outlook.com>
2020-03-17 23:57:44 +0300

    update resume route, team work.

--- Server/app/routes/resume.routes.js
+++ Server/app/routes/resume.routes.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 // Require Mongoose Model for Resume
 const Resume = require('../models/resume.model');
+const User = require('../models/resume.model');
 
 // Instantiate a Router (mini app that only handles routes)
 const router = express.Router();
@@ -52,24 +53,96 @@ router.get('/resumes/:id', (req, res) => {
         })
 });
 
-/**
-* Action:       CREATE
-* Method:       POST
-* URI:          /resumes
-* Description:  Create a new Resume
-*/
-router.post('/resumes', (req, res) => {
-    Resume.create(req.body.resume)
-        // On a successful `create` action, respond with 201
-        // HTTP status and the content of the new resume.
-        .then((newResume) => {
-            res.status(201).json({ resume: newResume });
+///// Add New Resume to User Account /////
+router.post('/resume', (req, res) => {
+    let username;
+    // find a user based on the token 
+    User.findOne({ username: req.body.userSubmitBy.username })
+        .then(record => {
+            if (!record) {
+                res.status(401).json({
+                    error: {
+                        name: 'Unauthorized',
+                        message: 'The provided cridintials are not valid for this operation'
+                    }
+                })
+            }
+            else {
+                user = record
+                //create new resume
+                Resume.create(req.body.newResumeInfo.resume)
+                    .then((newResume) => {
+                        user.resume.push(newResume._id)
+                        user.save()
+                        res.status(201).json({
+                            "newResume": newResume
+                        });
+                    })
+            }
         })
         // Catch any errors that might occur
         .catch((error) => {
-            res.status(500).json({ error });
+            res.status(500).json({
+                error: error
+            });
         });
 });
+
+// // CREATE TWO INGREDIENTS
+// const cheddar = new Ingredient({
+//     name: 'cheddar cheese',
+//     origin: 'Wisconson'
+// });
+
+
+// // CREATE A NEW FOOD
+// const cheesyQuiche = new Food({
+//     name: 'Quiche',
+//     ingredients: []
+// });
+
+// // PUSH THE INGREDIENTS ONTO THE FOOD'S
+// // INGREDIENTS ARRAY
+// cheesyQuiche.ingredients.push(cheddar); // associated!
+// cheesyQuiche.save((err, savedFood) => {
+//     if (err) {
+//         return console.log(err);
+//     } else {
+//         console.log('cheesyQuiche food is ', savedFood);
+//     }
+// });
+
+
+// /**
+// * Action:       CREATE
+// * Method:       POST
+// * URI:          /resumes
+// * Description:  Create a new Resume
+// */
+// router.post('/resumes', (req, res) => {
+//     let username;
+//     //create new resume
+//     Resume.create(req.body.resume)
+//         // On a successful `create` action, respond with 201
+//         // HTTP status and the content of the new resume.
+//         .then((newResume) => {
+//             // SAVE newResume
+//             // WE HAVE ACCESS TO _ID
+//             newResume.save((err, savedIng) => {
+//                 if (err) {
+//                     return console.log(err);
+//                 } else {
+//                     console.log('cheddar saved successfully', savedIng);
+//                 }
+//             });
+//             Resume.Resume.push(cheddar); // associated!
+//             res.status(201).json({ resume: newResume });
+//         })
+//         // Catch any errors that might occur
+//         .catch((error) => {
+//             res.status(500).json({ error });
+//         });
+// });
 
 /**
  * Action:      UPDATE
