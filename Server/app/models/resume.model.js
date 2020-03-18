757b88545c549b054c7bc07f147ddea997722aff
Mohammed H. AlMuhanna <almuhannamh@outlook.com>
2020-03-18 11:30:48 +0300

    update the schema of resume, team work

--- Server/app/models/resume.model.js
+++ Server/app/models/resume.model.js
@@ -1,42 +1,35 @@
 const mongoose = require("mongoose");
 
 //Define User Schema 
-const ResumeSchema = new mongoose.Schema(
-    {
-        work: [{
-            workCompany: { type: String, required: true },
-            workPosition: { type: String, required: true },
-            workWebsite: { type: String },
-            workStartDate: { type: Date, required: true },
-            workEndDate: { type: Date, },
-            workSummary: { type: String, required: true },
-        }],
-        volunteer: [{
-            volunteerOrganization: { type: String },
-            volunteerPosition: { type: String },
-            volunteerWebsite: { type: String },
-            volunteerStartDate: { type: Date, },
-            volunteerEndDate: { type: Date },
-            volunteerSummary: { type: String },
-        }],
-        education: [{
-            educationInstitution: { type: String, required: true },
-            educationMajor: { type: String, required: true },
-            educationStudyType: { type: String, required: true },
-            educationStartDate: { type: Date, required: true },
-            educationEndDate: { type: Date, required: true },
-            educationGpa: Number,
-            educationSummary: { type: String },
-        }],
-        skills: [{
-            skillsName: { type: String },
-            skillsLevel: { type: String },
-        }],
-        languages: [{
-            languageName: { type: String },
-            languageFluency: { type: String }
-        }],
-    },
+const ResumeSchema = new mongoose.Schema({
+    workCompany: { type: String, required: true },
+    workPosition: { type: String, required: true },
+    workWebsite: { type: String },
+    workStartDate: { type: Date, required: true },
+    workEndDate: { type: Date, },
+    workSummary: { type: String, required: true },
+
+    volunteerOrganization: { type: String },
+    volunteerPosition: { type: String },
+    volunteerWebsite: { type: String },
+    volunteerStartDate: { type: Date, },
+    volunteerEndDate: { type: Date },
+    volunteerSummary: { type: String },
+
+    educationInstitution: { type: String, required: true },
+    educationMajor: { type: String, required: true },
+    educationStudyType: { type: String, required: true },
+    educationStartDate: { type: Date, required: true },
+    educationEndDate: { type: Date, required: true },
+    educationGpa: Number,
+    educationSummary: { type: String },
+
+    skillsName: { type: String },
+    skillsLevel: { type: String },
+    languageName: { type: String },
+    languageFluency: { type: String }
+
+},
     { timestamps: true }
 );
 
